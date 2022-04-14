b7c340e83cfc80a286154a94caa2fbdb6a1fc56f
David Moles <dmoles@berkeley.edu>
2022-04-14 16:40:18 -0700

    Link terms to items

--- app/models/term.rb
+++ app/models/term.rb
@@ -1,14 +1,28 @@
 # A controlled vocabulary term.
 class Term < ApplicationRecord
-  belongs_to :facet
+
+  # ------------------------------------------------------------
+  # Relations
+
   has_many :children, class_name: 'Term', foreign_key: :parent_id, inverse_of: :parent, dependent: :destroy
   belongs_to :parent, class_name: 'Term', optional: true
+  has_and_belongs_to_many :items
+  belongs_to :facet
+
+  # ------------------------------------------------------------
+  # Validations
 
   validates :value, presence: true
   validates :value, uniqueness: true
   validate :parent_is_same_facet
 
+  # ------------------------------------------------------------
+  # Private methods
+
   private
+
+  # ------------------------------
+  # Validators
 
   def parent_is_same_facet
     return if parent.nil? || facet == parent.facet
