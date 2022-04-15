ae8373cdd3cf82b9ad5457b37c0a0d8e8e00efa6
David Moles <dmoles@berkeley.edu>
2022-04-15 11:44:22 -0700

    Drop facet attributes

---
+++ db/migrate/20220415173214_drop_facet_attributes.rb
@@ -0,0 +1,32 @@
+class DropFacetAttributes < ActiveRecord::Migration[7.0]
+  def up
+    remove_columns :items, :colors, :size, :decade, :genre, :medium
+  end
+
+  def down
+    add_column :items, :colors, :string
+    add_column :items, :size, :string
+    add_column :items, :decade, :string
+    add_column :items, :genre, :string
+    add_column :items, :medium, :string
+
+    facets_by_id = Facet.eager_load(:terms).each_with_object({}) do |f, ff|
+      ff[f.id] = f
+    end
+
+    Item.eager_load(:terms).find_each do |item|
+      term_vals_by_facet = item.terms.each_with_object({}) do |t, tv|
+        facet = facets_by_id[t.facet_id]
+        (tv[facet] ||= []) << t.value
+      end
+      next if term_vals_by_facet.empty?
+
+      item_attrs = term_vals_by_facet.each_with_object({}) do |(f, tv), attrs|
+        attr_name = f.name.downcase.to_sym
+        attrs[attr_name] = tv.join(', ')
+      end
+
+      item.update!(**item_attrs)
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_04_14_225809) do
+ActiveRecord::Schema[7.0].define(version: 2022_04_15_173214) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -27,13 +27,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_04_14_225809) do
     t.string "artist"
     t.string "artist_url"
     t.string "date"
-    t.string "decade"
     t.string "description"
-    t.string "medium"
-    t.string "colors"
-    t.string "genre"
     t.string "dimensions"
-    t.string "size"
     t.string "series"
     t.string "mms_id"
     t.string "barcode"
