e2e6b1d20ba5bd9ea7eb6271abcf94c06cb96597
David Moles <dmoles@berkeley.edu>
2022-04-18 11:08:07 -0700

    Simplify JSONAPI response matching

--- spec/support/matchers/jsonapi_matchers.rb
+++ spec/support/matchers/jsonapi_matchers.rb
@@ -3,11 +3,9 @@ require_relative 'hash_diff_matcher'
 module BerkeleyLibrary
   module Matchers
 
-    def contain_jsonapi_for(expected_obj)
-      return JsonApiErrorMatcher.new(expected_obj) if expected_obj.is_a?(ActiveModel::Error)
-      return JsonApiModelMatcher.new(expected_obj) if expected_obj.is_a?(ActiveRecord::Base)
-
-      raise ArgumentError, "Don't know how to match JSONAPI for #{expected_obj}"
+    def contain_jsonapi_for(expected_obj, options = {})
+      matcher_class = matcher_class_for(expected_obj)
+      matcher_class.new(expected_obj, options)
     end
 
     def jsonapi_for(expected_obj)
@@ -17,32 +15,20 @@ module BerkeleyLibrary
     class JsonApiMatcherBase < HashDiffMatcher
       attr_reader :expected_obj
 
-      def initialize(expected_obj, strict: true)
-        expected_hash = expected_hash_for(expected_obj)
+      def initialize(expected_obj, options = {}, strict: true)
+        expected_hash = expected_hash_for(expected_obj, options)
         @expected_obj = expected_obj
         super(expected_hash, strict: strict)
-      end
-
-      protected
-
-      def failure_message_base
-        "Expected JSON:API representation of #{expected_obj}, got #{actual.inspect}: "
       end
     end
 
     class JsonApiModelMatcher < JsonApiMatcherBase
-      def expected_hash_for(expected_obj)
-        raise ArgumentError, "Not an #{ActiveRecord::Base}: #{expected_obj.inspect}" unless model_like?(expected_obj)
-
-        {
-          'id' => expected_obj.id.to_s,
-          'type' => expected_obj.class.name.underscore,
-          'attributes' => expected_obj.attributes.except('id').transform_values(&:as_json)
-        }
-      end
-
-      def model_like?(expected_model)
-        %i[id attributes].all? { |attr| expected_model.respond_to?(attr) }
+      def expected_hash_for(expected_obj, options = {})
+        collection_like = JSONAPI::Rails.is_collection?(expected_obj)
+        serializer_class = JSONAPI::Rails.serializer_class(expected_obj, collection_like)
+        serializer = serializer_class.new(expected_obj, options)
+        h_raw = serializer.serializable_hash
+        JSON.parse(h_raw.to_json)
       end
     end
 
@@ -50,11 +36,11 @@ module BerkeleyLibrary
 
       attr_reader :expected_error
 
-      def initialize(expected_obj)
-        super(expected_obj, strict: false)
+      def initialize(expected_obj, options = {})
+        super(expected_obj, options, strict: false)
       end
 
-      def expected_hash_for(expected_error)
+      def expected_hash_for(expected_error, _options = {})
         raise ArgumentError, "Not an #{ActiveModel::Error}: #{expected_error.inspect}" unless validation_error_like?(expected_error)
 
         {
@@ -67,5 +53,16 @@ module BerkeleyLibrary
         %i[type attribute].all? { |attr| expected_error.respond_to?(attr) }
       end
     end
+
+    private
+
+    def matcher_class_for(expected_obj)
+      test_val = expected_obj.respond_to?(:first) ? expected_obj.first : expected_obj
+      return JsonApiErrorMatcher if test_val.is_a?(ActiveModel::Error)
+      return JsonApiModelMatcher if test_val.is_a?(ActiveRecord::Base)
+
+      raise ArgumentError, "Don't know how to match JSONAPI for #{expected_obj}"
+    end
+
   end
 end
