54b6543b8fa2ec4d4c8bf86194bf82791a8f802f
David Moles <dmoles@berkeley.edu>
2022-04-18 11:57:29 -0700

    Add specs for serializers

---
+++ spec/serializers/facet_serializer_spec.rb
@@ -0,0 +1,41 @@
+require 'rails_helper'
+
+describe FacetSerializer do
+  before do
+    # creating the terms creates the facets by side effect
+    FactoryBot.factories.select { |f| f.build_class == Term }.each { |f| create(f.name) }
+  end
+
+  it 'serializes a facet' do
+    facet = create(:facet)
+    expected_hash = {
+      data: {
+        id: facet.id.to_s,
+        type: :facet,
+        attributes: { name: facet.name },
+        relationships: {
+          terms: {
+            data: facet.terms.map { |t| { id: t.id.to_s, type: :term } }
+          }
+        }
+      }
+    }
+
+    serializer = FacetSerializer.new(facet)
+    actual_hash = serializer.serializable_hash
+    expect(actual_hash).to deep_eq_hash(expected_hash)
+  end
+
+  it 'can include the terms' do
+    facet = create(:facet_medium)
+    serializer = FacetSerializer.new(facet, { include: [:terms] })
+    actual_hash = serializer.serializable_hash
+
+    expected_includes = facet.terms.map { |t| TermSerializer.new(t).serializable_hash[:data] }
+    actual_includes = actual_hash[:included]
+    expect(actual_includes.size).to eq(expected_includes.size)
+    expected_includes.each_with_index do |incl, i|
+      expect(actual_includes[i]).to deep_eq_hash(incl)
+    end
+  end
+end
