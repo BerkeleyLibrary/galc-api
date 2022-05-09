8487d6826067ef2a1571dd65bbf4e75881b2989c
David Moles <dmoles@berkeley.edu>
2022-05-09 16:23:40 -0700

    Don't serialize empty relationships

--- app/serializers/term_serializer.rb
+++ app/serializers/term_serializer.rb
@@ -6,4 +6,15 @@ class TermSerializer
   belongs_to :facet
   belongs_to :parent, serializer: TermSerializer
   has_many :children, serializer: TermSerializer
+
+  class << self
+    def relationships_hash(record, relationships = nil, fieldset = nil, includes_list = nil, params = nil)
+      # noinspection RubyMismatchedReturnType
+      super.filter_map do |rel, h|
+        next if h[:data].blank?
+
+        [rel, h]
+      end.to_h
+    end
+  end
 end
