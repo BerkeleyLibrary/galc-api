11a8bf4fa3bfa862a7f18f1b04159805d402eb8f
David Moles <dmoles@berkeley.edu>
2022-08-10 11:53:20 -0700

    Drop test item

---
+++ db/migrate/20220810184802_drop_test_item.rb
@@ -0,0 +1,38 @@
+# Ensure migration can run without error even if we delete/rename the models
+(class Facet < ActiveRecord::Base; end) unless defined?(Facet)
+(class Item < ActiveRecord::Base; end) unless defined?(Item)
+(class Term < ActiveRecord::Base; end) unless defined?(Term)
+
+class DropTestItem < ActiveRecord::Migration[7.0]
+
+  ITEM_ATTRS = {
+    image: "cooper_mouth.jpg",
+    title: "Alvin's Test Record",
+    artist: "Alvin Pollock",
+    date: "Oct 2020",
+    description: "Kitty picture",
+    thumbnail: "cooper_mouth_360px.jpg",
+    suppressed: true
+  }
+
+  ITEM_TERMS = {
+    'Appearance' => 'Color',
+    'Size' => 'Small',
+    'Decade' => 'After 1999',
+    'Genre' => 'Animals',
+    'Medium' => 'Photograph'
+  }
+
+  def up
+    item = Item.find_by!(ITEM_ATTRS)
+    item.destroy!
+  end
+
+  def down
+    item = Item.create!(ITEM_ATTRS)
+    item.terms = ITEM_TERMS.map do |fn, tv|
+      facet = Facet.find_by!(name: fn)
+      facet.terms.where(value: tv).first
+    end
+  end
+end
