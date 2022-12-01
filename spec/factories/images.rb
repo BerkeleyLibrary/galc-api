e41eacfe09ef6442b6f4fd2def9e60291914f9cc
David Moles <dmoles@berkeley.edu>
2022-12-01 13:56:04 -0800

    Add destroy checks / hooks to Image

--- spec/factories/images.rb
+++ spec/factories/images.rb
@@ -3,6 +3,14 @@ FactoryBot.define do
     basename { 'Minami(Tree).jpg' }
     thumbnail { 'Minami(Tree)_360px.jpg' }
 
+    to_create do |instance|
+      instance.id = Image.find_or_create_by(
+        basename: instance.basename,
+        thumbnail: instance.thumbnail
+      ).id
+      instance.reload
+    end
+
     factory :image_reyes do
       basename { 'Reyes (Eastern Sierras Color).jpg' }
       thumbnail { 'Reyes (Eastern Sierras Color)_360px.jpg' }
