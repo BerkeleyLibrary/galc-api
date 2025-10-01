926632d44f16bf61a23090e8d3bedc881c63a58d
zhouyu <zhouyu@berkeley.edu>
2025-10-01 14:26:46 -0700

    Fix the special items

--- db/migrate/20250930230620_update_fix_term_item_relation.rb
+++ db/migrate/20250930230620_update_fix_term_item_relation.rb
@@ -1,46 +1,47 @@
+# Update terms of the three items whose value is nil or not in date or year format
 class UpdateFixTermItemRelation < ActiveRecord::Migration[7.0]
   def up
-    after_1999_id = select_value("SELECT id FROM terms WHERE value = 'After 1999'")
-    after_2020_id = select_value("SELECT id FROM terms WHERE value = 'After 2020'")
-    return unless after_1999_id && after_2020_id
-    
-    execute <<~SQL.squish
-      WITH target AS (
-        SELECT it.term_id, it.item_id
-        FROM items i
-        JOIN items_terms it ON i.id = it.item_id
-        JOIN terms t ON it.term_id = t.id
-        WHERE t.id = #{after_1999_id}
-          AND i.date = '[2020]'
-      )
-      UPDATE items_terms it
-      SET term_id = #{after_2020_id}
-      FROM target
-      WHERE it.item_id = target.item_id
-      AND it.term_id = target.term_id;
-    SQL
+    facet = Facet.find_by!(name: 'Decade')
+    t_after_1999 = Term.find_by!(facet: facet, value: 'After 1999')
+    t2000_2009 = Term.find_by!(facet: facet, value: '2000-2009')
+    t2020_plus = Term.find_by!(facet: facet, value: 'After 2020')
+
+    # Items with nil date
+    insert_and_delete(t_after_1999.id, t2000_2009.id, "i.date is null AND i.title in ('Loading a Cannon', 'Dense Populations')")
+    # Items with date = '[2020]'
+    insert_and_delete(t_after_1999.id, t2020_plus.id, "date = '[2020]'")
   end
 
   def down
-    after_1999_id = select_value("SELECT id FROM terms WHERE value = 'After 1999'")
-    after_2020_id = select_value("SELECT id FROM terms WHERE value = 'After 2020'")
-    return unless after_1999_id && after_2020_id
+    facet = Facet.find_by!(name: 'Decade')
+    t_after_1999 = Term.find_by!(facet: facet, value: 'After 1999')
+    t2000_2009 = Term.find_by!(facet: facet, value: '2000-2009')
+    t2020_plus = Term.find_by!(facet: facet, value: 'After 2020')
+
+    # revert items with nil date
+    insert_and_delete(t2000_2009.id,t_after_1999.id, "i.date is null AND i.title in ('Loading a Cannon', 'Dense Populations')")
+    # revert items with date = '[2020]'
+    insert_and_delete(t2020_plus.id, t_after_1999.id, "date = '[2020]'")
+  end
+
+  private
+
+  def insert_and_delete(old_term_id, new_term_id, condition_sql)
 
     execute <<~SQL.squish
       WITH target AS (
-        SELECT it.term_id, it.item_id
+        SELECT it.term_id, it.item_id, i.title
         FROM items i
         JOIN items_terms it ON i.id = it.item_id
         JOIN terms t ON it.term_id = t.id
-        WHERE t.id = #{after_2020_id}
-          AND i.date = '[2020]'
+        WHERE t.id = #{old_term_id}
+          AND  #{condition_sql}
       )
       UPDATE items_terms it
-      SET term_id = #{after_1999_id}
+      SET term_id = #{new_term_id}
       FROM target
       WHERE it.item_id = target.item_id
       AND it.term_id = target.term_id;
     SQL
   end
-
 end
